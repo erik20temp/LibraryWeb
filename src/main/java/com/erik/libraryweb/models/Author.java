02bcd8f0c79246768639bf856f8e2c9052a5b75f
Irek Wyka <ikwa65@gmail.com>
2021-06-02 20:17:20 +0200

    build Author class

--- src/main/java/com/erik/libraryweb/models/Author.java
+++ src/main/java/com/erik/libraryweb/models/Author.java
@@ -1,4 +1,84 @@
 package com.erik.libraryweb.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Author {
+
+    private Long id;
+    private String authorFirstName;
+    private String authorLastName;
+
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(Long id, String authorFirstName, String authorLastName) {
+        this.id = id;
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+    }
+
+    public Author(String authorFirstName, String authorLastName) {
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", authorFirstName='" + authorFirstName + '\'' +
+                ", authorLastName='" + authorLastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
