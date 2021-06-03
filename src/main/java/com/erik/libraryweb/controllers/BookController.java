33bcf7f36c31a05c41a5f97650dacba34903210f
Irek Wyka <ikwa65@gmail.com>
2021-06-03 19:19:40 +0200

    add package controllers with RestController for each class

---
+++ src/main/java/com/erik/libraryweb/controllers/BookController.java
@@ -0,0 +1,9 @@
+package com.erik.libraryweb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "books")
+public class BookController {
+}
