e9227044c69d44ae62cdb5a491f9d4d50247ca50
Irek Wyka <ikwa65@gmail.com>
2021-06-04 19:30:40 +0200

    add repositories and build constructor

--- src/main/java/com/erik/libraryweb/seeds/SeedData.java
+++ src/main/java/com/erik/libraryweb/seeds/SeedData.java
@@ -2,16 +2,22 @@ package com.erik.libraryweb.seeds;
 
 import com.erik.libraryweb.repositories.AuthorRepository;
 import com.erik.libraryweb.repositories.BookRepository;
+import com.erik.libraryweb.repositories.PublisherRepository;
+import com.erik.libraryweb.repositories.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class SeedData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+    private final StudentRepository studentRepository;
 
-    public SeedData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public SeedData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository, StudentRepository studentRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+        this.studentRepository = studentRepository;
     }
 
     @Override
