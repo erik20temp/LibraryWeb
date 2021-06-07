40cf80480f126487f45a752424b75e705984d22c
Irek Wyka <ikwa65@gmail.com>
2021-06-07 20:09:58 +0200

    add view author template list.html

--- src/main/java/com/erik/libraryweb/controllers/AuthorController.java
+++ src/main/java/com/erik/libraryweb/controllers/AuthorController.java
@@ -1,9 +1,23 @@
 package com.erik.libraryweb.controllers;
 
+import com.erik.libraryweb.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "authors")
+@Controller
 public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping(path = "/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+
 }
--- src/main/java/com/erik/libraryweb/services/AuthorService.java
+++ src/main/java/com/erik/libraryweb/services/AuthorService.java
@@ -2,6 +2,7 @@ package com.erik.libraryweb.services;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class AuthorService {
 }
